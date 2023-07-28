d72cc3d732f0af08c4d038783902e54ec1eb6bb6
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-07-28 19:49:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div className="card-inside">
+          <p className="card-label">FRIO</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
