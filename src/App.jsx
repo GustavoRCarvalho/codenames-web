92a927ab6cccd4895132414ed4fa87bdea46d6eb
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-07-29 01:03:10 -0300

    add: random words

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import "./App.css"
 import { CardBackground } from "./components/Card/CardBackground"
 import { Grid } from "./components/Screen/Grid"
 import { ShowAllButton } from "./components/Screen/ShowAllButton"
 import { TipsBackground } from "./components/TeamsTips.jsx/TipsBackground"
 import { styled } from "styled-components"
+import { words } from "./assets/words"
+import { getRandomArbitrary } from "./assets/utils"
 
 function App() {
   const [selected, setSelected] = useState([])
   const [showAll, setShowAll] = useState(false)
 
-  const words = [
-    { label: "Vento", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Praia", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Chapéu", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Cachorro", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Pincel", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Leão", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Barco", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Abacaxi", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Trem", color: "#121212", bordercolor: "#505050" },
-    { label: "Cachecol", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Helicóptero", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Lápis", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Esquilo", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Relógio", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Girafa", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Limão", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Caranguejo", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Pintura", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Piano", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Montanha", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Sapatilha", color: "#3aa4ff", bordercolor: "#2967b4" },
-    { label: "Teatro", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Computador", color: "#fff", bordercolor: "#2c2c2c" },
-    { label: "Melancia", color: "#eb37bc", bordercolor: "#b4298f" },
-    { label: "Mariposa", color: "#3aa4ff", bordercolor: "#2967b4" },
-  ]
+  const wordList = useMemo(() => {
+    return [
+      ...Array.apply(null, Array(9)).map(() => {
+        return {
+          label: words[getRandomArbitrary(0, words.length)],
+          color: "#eb37bc",
+          bordercolor: "#b4298f",
+        }
+      }),
+      ...Array.apply(null, Array(8)).map(() => {
+        return {
+          label: words[getRandomArbitrary(0, words.length)],
+          color: "#3aa4ff",
+          bordercolor: "#2967b4",
+        }
+      }),
+      ...Array.apply(null, Array(7)).map(() => {
+        return {
+          label: words[getRandomArbitrary(0, words.length)],
+          color: "#fff",
+          bordercolor: "#2c2c2c",
+        }
+      }),
+      {
+        label: words[getRandomArbitrary(0, words.length)],
+        color: "#121212",
+        bordercolor: "#505050",
+      },
+    ]
+  }, [])
 
   return (
     <>
@@ -44,7 +49,7 @@ function App() {
       <ContentContainer>
         <TipsBackground color={"#eb37bc"} />
         <Grid>
-          {words.map((card, index) => {
+          {wordList.map((card, index) => {
             return (
               <CardBackground
                 key={index}
