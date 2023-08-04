6367c192daf80d5baf1f5b9c26b06ee8c767df7f
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-08-04 18:28:43 -0300

    add: Help and Contact button

--- src/components/Screens/Menu.jsx
+++ src/components/Screens/Menu.jsx
@@ -1,25 +1,42 @@
 import { styled } from "styled-components"
-import { CommonButton } from "../common/CommonButton"
 import { ContentContainer } from "../common/ContentLimit"
+import { ButtonLinkRouter } from "../common/ButtonLinkRouter"
 import { NoStyleLinkRouter } from "../common/NoStyleLinkRouter"
+import { CommonButton } from "../common/CommonButton"
 
 export const Menu = () => {
   return (
     <MenuContainer>
       <NoStyleLinkRouter to="online">
-        <CommonButton>Jogar Online</CommonButton>
+        <Button type="button">Jogar Online</Button>
       </NoStyleLinkRouter>
       <NoStyleLinkRouter to="offline">
-        <CommonButton>Jogar Offline</CommonButton>
+        <Button type="button">Jogar Offline</Button>
+      </NoStyleLinkRouter>
+      <NoStyleLinkRouter to="help">
+        <Button type="button">Como Jogar</Button>
+      </NoStyleLinkRouter>
+      <NoStyleLinkRouter to="contact">
+        <Button type="button">Quem Somos</Button>
       </NoStyleLinkRouter>
     </MenuContainer>
   )
 }
 
+export const Button = styled(CommonButton)`
+  margin: 0em;
+  width: 100%;
+`
+
 const MenuContainer = styled(ContentContainer)`
   height: 100vh;
 
-  display: flex;
+  display: grid;
+  align-content: center;
   align-items: center;
+
+  gap: 2em;
+  grid-template-columns: 12em 12em;
+  grid-template-rows: 3em 3em;
   justify-content: center;
 `
